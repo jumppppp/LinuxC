8d3449e0fb0f3d11905126393539627a2fefabc6
jumppppp <2817797085@qq.com>
2023-03-03 13:45:42 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   10/posix
    	modified:   10/posix.c
    	modified:   10/readme

--- 10/posix.c
+++ 10/posix.c
@@ -3,15 +3,19 @@
 #include <pthread.h>
 #include <string.h>
 #define LEFT 30000000
-#define RIGHT 30005000
-#define MAX_P 65535
+#define RIGHT 30050000
+#define MAX_P 51200
 
-static pthread_t *plist[MAX_P];
-
+static pthread_t plist[MAX_P];
+struct thr_arg
+{
+    int arg;
+};
 static void *thr_prime(void *p)
 {
-    int mark =1;
-    int i = *(int *)p;
+    int mark = 1;
+    int i = ((struct thr_arg *)p)->arg;
+
     for (int j = 2; j < i / 2; j++)
     {
         if (i % j == 0)
@@ -22,15 +26,17 @@ static void *thr_prime(void *p)
     }
     if (mark)
     {
-        printf("%d", i);
+        printf("%d\n", i);
     }
-    pthread_exit(NULL);
+    pthread_exit(p);
 }
 static int find_pos(void)
 {
     for (int i = 0; i < MAX_P; i++)
     {
-        if (plist[i] == NULL)
+
+        // printf("<%d>",plist[i]);
+        if (plist[i] == 0)
         {
             return i;
         }
@@ -39,32 +45,40 @@ static int find_pos(void)
 }
 int main()
 {
-    
-    printf("<<%d>>", 1);
+
     int err;
     int pos;
-    pthread_t *tid=NULL;
-    printf("<<%d>>",2);
+    struct thr_arg *p;
+    void * ptr;
     for (int i = LEFT; i <= RIGHT; i++)
     {
-        err = pthread_create(tid, NULL, thr_prime, &i);
+        p = malloc(sizeof(*p));
+        if (p==0){
+                exit(1);
+        }
+        p->arg = i;
+        pthread_t tid;
+        err = pthread_create(&tid, NULL, thr_prime, p);
         if (err)
         {
             fprintf(stderr, "pthread_create():%s\n", strerror(err));
+            printf("count={{%d}}\n", i);
             exit(1);
         }
         pos = find_pos();
-        printf("<<%d>>", pos);
+        // printf("<%d>", pos);
         if (pos == -1)
         {
             printf("find_pos_no!");
             exit(1);
         }
         plist[pos] = tid;
+        // printf("[%d]",plist[pos]);
     }
-    for (int i = 0; plist[i] == NULL; i++)
+    for (int i = 0; plist[i] == 0; i++)
     {
-        pthread_join(*plist[i], NULL);
+        pthread_join(plist[i], &ptr);
+        free(ptr);
     }
     exit(0);
 }
