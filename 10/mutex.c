45cfa5f8c2654334600eb7fb151fe0fd509c3350
jumppppp <2817797085@qq.com>
2023-03-03 16:36:48 +0800

    Changes to be committed: 	modified:   10/mutex 	modified:   10/mutex.c 	modified:   10/tmp1

--- 10/mutex.c
+++ 10/mutex.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define TMAX 100
+#include<unistd.h>
+#define TMAX 11
 #define FILENAME "tmp1"
+
 static pthread_t plist[TMAX];
+static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 static void *func1(void *)
 {
     char linebuf[1024];
@@ -12,10 +15,14 @@ static void *func1(void *)
     if(fp ==NULL){
         exit(1);
     }
+    pthread_mutex_lock(&mut);
     fgets(linebuf,1024,fp);
     fseek(fp,0,SEEK_SET);
     fprintf(fp,"%d",(atoi(linebuf)+1));
     fclose(fp);
+    sleep(1);
+    printf(".");
+    pthread_mutex_unlock(&mut);
     pthread_exit(NULL);
 }
 int main()
@@ -34,6 +41,6 @@ int main()
     {
         pthread_join(plist[i], NULL);
     }
-
+    pthread_mutex_destroy(&mut);
     exit(0);
 }
