abae1e7297c7bf6fa429f7282cf3609cf9fb2451
jumppppp <2817797085@qq.com>
2023-03-06 14:08:02 +0800

    Changes not staged for commit: 	deleted:    11/mytbuf_mt/main 	deleted:    11/mytbuf_mt/main.c 	deleted:    11/mytbuf_mt/main_o3 	deleted:    11/mytbuf_mt/mytbf.c 	deleted:    11/mytbuf_mt/mytbf.h 	deleted:    11/mytbuf_mt/run 	modified:   11/readme 	deleted:    8/mytbuf/main 	deleted:    8/mytbuf/main.c 	deleted:    8/mytbuf/main_o3 	deleted:    8/mytbuf/mytbf.c 	deleted:    8/mytbuf/mytbf.h 	deleted:    8/mytbuf/run 	deleted:    9/mytbuf_sa/main 	deleted:    9/mytbuf_sa/main.c 	deleted:    9/mytbuf_sa/main_o3 	deleted:    9/mytbuf_sa/mytbf.c 	deleted:    9/mytbuf_sa/mytbf.h 	deleted:    9/mytbuf_sa/run
    Untracked files:
    	11/mytbf_mutex_thread/
    	8/mytbuf_signal/
    	9/mytbf_sigaction/

--- 11/mytbuf_mt/mytbf.c
+++ 11/mytbuf_mt/mytbf.c
@@ -39,70 +39,93 @@ static void module_unload(void)
 
 	// signal(SIGALRM, alarm_save);
 	// alarm(0);
-	struct sigaction sa;
-	struct itimerval itv;
-	int ret;
-	ret = sigaction(SIGALRM, &al_save, NULL);
-	if (ret < 0)
-	{
-		fprintf(stderr, "sigaction()");
-	}
-	/* if error*/
-	itv.it_interval.tv_sec = 0;
-	itv.it_interval.tv_usec = 0;
-	itv.it_value.tv_sec = 0;
-	itv.it_value.tv_usec = 0;
-	ret = setitimer(ITIMER_REAL, &itv, NULL);
-	if (ret < 0)
-	{
-		fprintf(stderr, "sigemptyset()");
-	}
+	// struct sigaction sa;
+	// struct itimerval itv;
+	// int ret;
+	// ret = sigaction(SIGALRM, &al_save, NULL);
+	// if (ret < 0)
+	// {
+	// 	fprintf(stderr, "sigaction()");
+	// }
+	// /* if error*/
+	// itv.it_interval.tv_sec = 0;
+	// itv.it_interval.tv_usec = 0;
+	// itv.it_value.tv_sec = 0;
+	// itv.it_value.tv_usec = 0;
+	// ret = setitimer(ITIMER_REAL, &itv, NULL);
+	// if (ret < 0)
+	// {
+	// 	fprintf(stderr, "sigemptyset()");
+	// }
 	for (int i = 0; i < MYTBF_MAX; i++)
-		free(job[i]);
+	{
+		if (job[i]!=NULL){
+			pthread_mutex_destroy(&job[i]->mut);
+			free(job[i]);
+		}
+
+	}
+
 	pthread_mutex_destroy(&mut_job);
 }
 
-static void h1(int s, siginfo_t *infop, void *unuse)
+static void thr_alarm(void *)
 {
 	// alarm(1);
-	if (infop->si_code != SI_KERNEL)
-		return;
-	for (int i = 0; i < MYTBF_MAX; i++)
-	{
-		if (job[i] != NULL)
-		{
-			job[i]->token += job[i]->cps;
-			if (job[i]->token > job[i]->burst)
-			{
+	// if (infop->si_code != SI_KERNEL)
+	// 	return;
 
-				job[i]->token = job[i]->burst;
+	while (1)
+	{
+		for (int i = 0; i < MYTBF_MAX; i++)
+		{
+			pthread_mutex_lock(&mut_job);
+			if (job[i] != NULL)
+			{	
+				pthread_mutex_lock(&job[i]->mut);
+				job[i]->token += job[i]->cps;
+				if (job[i]->token > job[i]->burst)
+				{
+					job[i]->token = job[i]->burst;
+				}
+				pthread_mutex_unlock(&job[i]->mut);
 			}
+			pthread_mutex_unlock(&mut_job);
 		}
+		sleep(1);
 	}
 }
-
 static void module_load(void)
 {
-	struct sigaction sa;
-	struct itimerval itv;
-	int ret;
-	sa.sa_sigaction = h1;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
-	ret = sigaction(SIGALRM, &sa, &al_save);
-	if (ret < 0)
+
+	pthread_t tid_alarm;
+	int err;
+	err = pthread_create(&tid_alarm, NULL, thr_alarm, NULL);
+	if (err)
 	{
-		fprintf(stderr, "sigaction()");
+		fprintf(stderr, "pthread_create():%s\n", strerror(err));
+		exit(1);
 	}
-	itv.it_interval.tv_sec = 1;
-	itv.it_interval.tv_usec = 0;
-	itv.it_value.tv_sec = 1;
-	itv.it_value.tv_usec = 0;
-	ret = setitimer(ITIMER_REAL, &itv, NULL);
-	if (ret < 0)
-	{
-		fprintf(stderr, "setitimer");
-	}
+	// struct sigaction sa;
+	// struct itimerval itv;
+	// int ret;
+	// sa.sa_sigaction = h1;
+	// sigemptyset(&sa.sa_mask);
+	// sa.sa_flags = SA_SIGINFO;
+	// ret = sigaction(SIGALRM, &sa, &al_save);
+	// if (ret < 0)
+	// {
+	// 	fprintf(stderr, "sigaction()");
+	// }
+	// itv.it_interval.tv_sec = 1;
+	// itv.it_interval.tv_usec = 0;
+	// itv.it_value.tv_sec = 1;
+	// itv.it_value.tv_usec = 0;
+	// ret = setitimer(ITIMER_REAL, &itv, NULL);
+	// if (ret < 0)
+	// {
+	// 	fprintf(stderr, "setitimer");
+	// }
 	// alarm_save = signal(SIGALRM, h1);
 	// alarm(1);
 	atexit(module_unload);
@@ -126,13 +149,13 @@ mytbf_t *mytbf_init(int cps, int burst)
 	me->cps = cps;
 	me->burst = burst;
 	me->pos = pos;
-	pthread_mutex_init(&me->mut,NULL);
+	pthread_mutex_init(&me->mut, NULL);
 	pthread_mutex_lock(&mut_job);
 	pos = get_free_pos_unlocked();
 	if (pos < 0)
-	free(me);
-		pthread_mutex_unlock(&mut_job);
-		return NULL;
+		free(me);
+	pthread_mutex_unlock(&mut_job);
+	return NULL;
 	job[pos] = me;
 	pthread_mutex_unlock(&mut_job);
 	return me;
@@ -153,10 +176,18 @@ int mytbf_fetchtoken(mytbf_t *ptr, int size)
 	{
 		return -EINVAL;
 	}
+	pthread_mutex_lock(&me->mut);
 	while (me->token <= 0)
-		pause();
+	{
+		pthread_mutex_unlock(&me->mut);
+		sched_yield();
+		// pause();
+		pthread_mutex_lock(&me->mut);
+	}
+
 	n = min(me->token, size);
 	me->token -= n;
+	pthread_mutex_unlock(&me->mut);
 	return n;
 }
 int mytbf_returntoken(mytbf_t *ptr, int size)
@@ -167,11 +198,13 @@ int mytbf_returntoken(mytbf_t *ptr, int size)
 	{
 		return -EINVAL;
 	}
+	pthread_mutex_lock(&me->mut);
 	me->token += size;
 	if (me->token > me->burst)
 	{
 		me->token = me->burst;
 	}
+	pthread_mutex_unlock(&me->mut);
 	return size;
 }
 
@@ -182,6 +215,7 @@ int mytbf_destroy(mytbf_t *ptr)
 	pthread_mutex_lock(&mut_job);
 	job[me->pos] = NULL;
 	pthread_mutex_unlock(&mut_job);
+	pthread_mutex_destroy(&me->mut);
 	free(ptr);
 	return 0;
 }
